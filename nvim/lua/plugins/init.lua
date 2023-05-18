e4d68429bd32598e1f508d4623a14ec29fa105d0
Adam T. Gautier <1045171+gautada@users.noreply.github.com>
2023-05-18 12:46:51 -0400

    Trying to cut out the default lua stuff, platform and what not

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -183,9 +183,9 @@ local default_plugins = {
 
       -- cmp sources plugins
       {
-        "saadparwaiz1/cmp_luasnip",
-        "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-nvim-lsp",
+        -- "saadparwaiz1/cmp_luasnip",
+        -- "hrsh7th/cmp-nvim-lua",
+        -- "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
       },
