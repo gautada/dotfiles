86797e51d4c4851294c675f0d05f60831317eb56
Adam T. Gautier <1045171+gautada@users.noreply.github.com>
2025-03-31 08:16:55 -0400

    Added lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -41,6 +41,9 @@ return {
       lspconfig.ruff.setup({
         capabilities = capabilities
       })
+      lspconfig.markdownlint.setup({
+        capabilities = capabilities
+      })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
