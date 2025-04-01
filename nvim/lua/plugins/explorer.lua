44d8549bc7ae95a45667cedefa76b33fd1117da4
Adam T. Gautier <1045171+gautada@users.noreply.github.com>
2025-04-01 14:58:59 -0400

    Added the plugins for explorer and ai

--- nvim/lua/plugins/explorer.lua
+++ nvim/lua/plugins/explorer.lua
@@ -12,15 +12,39 @@ return {
 	config = function()
 		require("neo-tree").setup({
 			filesystem = {
+				follow_current_file = true, -- Keep Neo-tree in sync with the open file
+				hijack_netrw = true, -- Replace netrw with Neo-tree
+				use_libuv_file_watcher = true, -- Auto-update when files change
+				bind_to_cwd = false, -- Allow independent navigation
+				cwd_target = {
+					sidebar = "global", -- Global navigation, not limited to PWD
+					current = "global",
+				},
 				filtered_items = {
 					visible = true, -- Show hidden files (dotfiles)
 					hide_dotfiles = false, -- Don't hide files starting with `.`
 					hide_gitignored = false, -- Optionally show .gitignored files
 				},
+				window = {
+					mappings = {
+						["h"] = "navigate_up", -- Press `h` to go up a directory (`..`)
+						-- ["l"] = "open", -- Press `l` to open a file/directory
+						-- ["<cr>"] = "open_and_close", -- Pressing Enter will open and close Neo-tree
+					},
+				},
+			},
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function()
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
 			},
 		})
 		-- vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", {})
-		-- vim.keymap.set("n", "<leader>e", ":Neotree buffers reveal float<CR>", {})
-		vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal float<CR>", {})
+		vim.keymap.set("n", "<leader>exp", ":Neotree buffers reveal float<CR>", {})
+		vim.keymap.set("n", "<leader>exf", ":Neotree filesystem reveal float<CR>", {})
+		vim.keymap.set("n", "<leader>exs", ":Neotree filesystem reveal left<CR>", {})
 	end,
 }
