1a37268e2adeeb28ace726c60ffdcfe3139a8d24
Adam T. Gautier <1045171+gautada@users.noreply.github.com>
2025-04-09 10:23:47 -0400

    Updated fuzzy search in nvim and the docker setup

--- nvim/lua/plugins/search.lua
+++ nvim/lua/plugins/search.lua
@@ -10,8 +10,8 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			-- *** NOT SURE WHAT THESE KEYMAPS ARE ***
 			-- vim.keymap.set("n", "<leader>fz", ":Telescope live_grep<cr>")
 			-- vim.keymap.set("n", "<leader>fo", ":Telescope oldfiles<cr>")
