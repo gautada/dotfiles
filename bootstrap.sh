f163a9f707135b942f4a181d1d4bf1a96f8983b7
Adam T. Gautier <1045171+gautada@users.noreply.github.com>
2025-09-28 21:58:09 -0400

    Added kube and confirm that bootstrap.sh is working

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-cd "${HOME}/.local/share/dotfiles/public"
-stow --dir="${HOME}/.local/share/dotfiles/public" --no-folding --target="${HOME}" --verbose --restow */
+cd "${HOME}/.local/share/dotfiles/public" || exit 1
+stow --dir="${HOME}/.local/share/dotfiles/public" --no-folding \
+     --target="${HOME}" --verbose --restow */
+
+PRIVATE_DIR="${HOME}/.local/share/dotfiles/private"
+if [ -d "${PRIVATE_DIR}" ] ; then 
+ cd "${PRIVATE_DIR}" || exit 2
+ stow --dir="${PRIVATE_DIR}" --no-folding \
+      --target="${HOME}" --verbose --restow */
+fi
