93712a2f89369862205ef8a6527d6fcbd105492b
Adam T. Gautier <1045171+gautada@users.noreply.github.com>
2025-12-27 08:39:18 -0500

    Updated the lua to resolve a globally installed treesitter

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,9 +5,13 @@
 -- ## Configure
 --
 -- ### Options
--- 
+--
 -- Vim tooling options not havingto do with specifific plugins.
 --
+-- Alpine: ensure system-wide plugins are visible (Alpine packages live here)
+vim.opt.packpath:prepend("/usr/share/nvim/site")
+vim.opt.runtimepath:prepend("/usr/share/nvim/site")
+
 -- ### lazy
 --
 -- Setup the Lava.nvim package manager.
